992eacd5c1ef04f29caacfc0a16322d5a4a58d46
Ferdinand Cuisin <fcuisin@gmail.com>
2020-01-20 07:57:18 +0100

    Implemented scraper

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "nokogiri"
+
 class RecipesController < ApplicationController
   def new
     if params[:search] == "" || params[:search] == nil
@@ -7,6 +10,20 @@ class RecipesController < ApplicationController
     else
       @search = params[:search].downcase
       flash[:success] = "Great move"
+      scraper
+    end
+  end
+
+  def scraper
+    html = open("http://www.marmiton.org/recettes/recherche.aspx?aqt=#{@search}").read
+
+    doc = Nokogiri::HTML(html, nil, "utf-8")
+    # 2. For the first five results
+    @results = []
+    doc.search(".recipe-card").first(5).each do |element|
+    # 3. Create recipe and store it in results
+      name = element.search('.recipe-card__title').text.strip
+      @results << Recipe.new(name: name)
     end
   end
 end
